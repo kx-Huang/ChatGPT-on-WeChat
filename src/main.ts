54f26cfe438b7fa9a645999ffc57fcc17e9ab0f1
Kexuan Huang <hkx@Kexuans-MacBook-Pro.local>
2024-05-30 23:56:47 -0700

    fix: duplicate respond on restart

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function main() {
     // scan QR code for login
     .on("scan", async (qrcode, status) => {
       const url = `https://wechaty.js.org/qrcode/${encodeURIComponent(qrcode)}`;
-      console.log(`💡 Scan QR Code to login: ${status}\n${url}`);
+      console.log(`💡 Scan QR Code in WeChat to login: ${status}\n${url}`);
       console.log(
         await QRCode.toString(qrcode, { type: "terminal", small: true })
       );
@@ -28,6 +28,12 @@ async function main() {
     // message handler
     .on("message", async (message: any) => {
       try {
+        // prevent accidentally respond to history chat on restart
+        // only respond to message later than chatbot start time
+        const msgDate = message.date();
+        if (msgDate.getTime() <= chatGPTBot.startTime.getTime()) {
+          return;
+        }
         console.log(`📨 ${message}`);
         // handle message for customized task handlers
         await chatGPTBot.onCustimzedTask(message);
